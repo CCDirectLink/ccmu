429a8bb24ce925fe687415b89ffb1defde42c11d
Keanu <keanutimmermans8@gmail.com>
2019-06-30 13:44:41 +0200

    Add files via upload

--- help.go
+++ help.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func printHelp() {
-	fmt.Println("usage: ccmu <install|uninstall|update|list|outdated|help>")
+	fmt.Println("These are the usable prefixes: ccmu <install|uninstall|update|list|outdated|help>")
 }
