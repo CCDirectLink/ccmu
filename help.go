ad0ea770547030d6468259dc74e0c6954b0c0da3
2767mr <2767mr@gmx.at>
2019-07-15 20:23:56 +0200

    Updated heko text

--- help.go
+++ help.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func printHelp() {
-	fmt.Println("Usage: ccmu [command] [options] <args...>")
+	fmt.Println("Usage: ccmu [options] [command] <args...>")
 	fmt.Println("")
 	fmt.Println("Options:")
 	fmt.Println("  --game <path>         Sets the game folder used for operations")
@@ -11,7 +11,7 @@ func printHelp() {
 	fmt.Println("Commands:")
 	fmt.Println("  install <mod name>    Installs one or more mods")
 	fmt.Println("  uninstall <mod name>  Uninstall one or more mods")
-	fmt.Println("  update <mod name>     Updates one or more mods")
+	fmt.Println("  update [mod name]     Updates one or more mods")
 	fmt.Println("  list                  Lists all mods that the tool knows about")
 	fmt.Println("  outdated              Show the names and versions of outdated mods")
 	fmt.Println("  version               Display the version of this tool")
