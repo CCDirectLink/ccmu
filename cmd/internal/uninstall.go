c8b0f68e4534bed99a296bd42241d5c53a54182f
2767mr <2767mr@gmx.at>
2019-12-26 12:52:30 +0100

    Implemented cmd.Uninstall

--- cmd/internal/uninstall.go
+++ cmd/internal/uninstall.go
@@ -1,6 +1,24 @@
 package internal
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/CCDirectLink/CCUpdaterCLI/pkg"
+)
+
 //Uninstall removes a mod from a directory
-func Uninstall(args []string) error {
-	return nil
+func Uninstall(args []string) {
+	for _, p := range getAll(args) {
+		err := p.Uninstall()
+		if err != nil {
+			var pkgErr pkg.Error
+			if errors.As(err, &pkgErr) {
+				fmt.Println(pkgErr.String())
+			} else {
+				fmt.Printf("An error occured in %s\n", err)
+			}
+			continue
+		}
+	}
 }
